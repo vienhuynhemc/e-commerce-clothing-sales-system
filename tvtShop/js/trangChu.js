467b043987b789e4f4c2f467b11e3e2fa5b51d70
vientamthuong <18130281@st.hcmuaf.edu.vn>
2020-11-10 02:20:26 +0700

    hoàn thành slider trang chủ, fix slider trở nên tuyệt vời rồirồi

---
+++ tvtShop/js/trangChu.js
@@ -0,0 +1,62 @@
+let countImg = 4;
+let now = 0;
+let slide = false;
+let lastTime = 0;
+
+function checkSlide() {
+    let date = new Date();
+    let time = date.getTime();
+    if (time - lastTime > 500) {
+        slide = false;
+        lastTime = time;
+    }
+}
+
+setInterval(() => {
+    checkSlide();
+    if (!slide) {
+        slide = true;
+        let step = 100 * countImg - 100;
+        if (countImg > 1) {
+            if (now >= step) {
+                now = 0;
+            } else {
+                now += 100;
+            }
+        }
+
+        document.getElementById('mainslider').style.left = `-${now}%`;
+    }
+}, 3000);
+
+function leftslide() {
+    checkSlide();
+    if (!slide) {
+        slide = true;
+        let step = 100 * countImg - 100;
+        if (countImg > 1) {
+            if (now <= 0) {
+                now = step;
+            } else {
+                now -= 100;
+            }
+        }
+        document.getElementById('mainslider').style.left = `-${now}%`;
+    }
+}
+
+function rightslide() {
+    checkSlide();
+    if (!slide) {
+        slide = true;
+        let step = 100 * countImg - 100;
+        if (countImg > 1) {
+            if (now >= step) {
+                now = 0;
+            } else {
+                now += 100;
+            }
+        }
+        document.getElementById('mainslider').style.left = `-${now}%`;
+    }
+}
