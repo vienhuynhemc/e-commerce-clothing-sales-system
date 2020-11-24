06e03ef670b808c57ad9440443b85af7fd5779e1
vientamthuong <18130281@st.hcmuaf.edu.vn>
2020-11-24 12:42:26 +0700

    change password in account

--- tvtShop/js/informationCustomer.js
+++ tvtShop/js/informationCustomer.js
@@ -6,8 +6,28 @@ function loadIMG(event) {
     }
 }
 
-function removeAvatar(){
+function removeAvatar() {
     document.getElementById('avatar').src = 'img/user.jpg';
     document.getElementById('avatar2').src = 'img/user.jpg';
 }
 
+function gobackpassword() {
+    document.getElementById('changepassword').style.transform = 'scaleY(0)';
+    document.getElementById('changepassworddiv').style.opacity = '1';
+    document.getElementById('changepassworddiv').style.zIndex = '2';
+    document.getElementById('changepasswordsuccess').style.opacity = '0';
+    document.getElementById('changepasswordsuccess').style.zIndex = '1';
+}
+
+function gotosuccesspassword() {
+    document.getElementById('changepassworddiv').style.opacity = '0';
+    document.getElementById('changepassworddiv').style.zIndex = '1';
+    document.getElementById('changepassworddiv').style.transform = 'scaleY(0)';
+    document.getElementById('changepasswordsuccess').style.opacity = '1';
+    document.getElementById('changepasswordsuccess').style.zIndex = '2';
+}
+
+function doimatkhau() {
+    document.getElementById('changepassworddiv').style.transform = 'scaleY(1)';
+    document.getElementById('changepassword').style.transform = 'scaleY(1)';
+}
