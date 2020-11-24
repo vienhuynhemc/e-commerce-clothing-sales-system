14d49f8c5c37e2746dc66829e569e8821d55443b
vientamthuong <18130281@st.hcmuaf.edu.vn>
2020-11-24 21:48:15 +0700

    complete account

--- tvtShop/js/informationCustomer.js
+++ tvtShop/js/informationCustomer.js
@@ -1,3 +1,8 @@
+let countImg = 5;
+let now = 0;
+let slide = false;
+let lastTime = 0;
+
 function loadIMG(event) {
     if (event.target.files.length > 0) {
         var src = URL.createObjectURL(event.target.files[0]);
@@ -31,3 +36,85 @@ function doimatkhau() {
     document.getElementById('changepassworddiv').style.transform = 'scaleY(1)';
     document.getElementById('changepassword').style.transform = 'scaleY(1)';
 }
+
+function left() {
+    checkSlide();
+    if (!slide) {
+        slide = true;
+        let step = 33 * countImg - 100 + 1;
+        if (countImg > 1) {
+            console.log(now);
+            if (now <= 0) {
+                now = step;
+            } else {
+                now -= 33;
+            }
+        }
+        document.getElementById('listhinh').style.left = `-${now}%`;
+    }
+}
+
+function right() {
+    checkSlide();
+    if (!slide) {
+        slide = true;
+        let step = 33 * countImg - 100;
+        if (countImg > 1) {
+            if (now >= step) {
+                now = 0;
+            } else {
+                now += 33;
+            }
+        }
+        document.getElementById('listhinh').style.left = `-${now}%`;
+    }
+}
+
+function checkSlide() {
+    let date = new Date();
+    let time = date.getTime();
+    if (time - lastTime > 1000) {
+        slide = false;
+        lastTime = time;
+    }
+}
+
+setInterval(() => {
+    checkSlide();
+    if (!slide) {
+        slide = true;
+        let step = 33 * countImg - 100;
+        if (countImg > 1) {
+            if (now >= step) {
+                now = 0;
+            } else {
+                now += 33;
+            }
+        }
+        document.getElementById('listhinh').style.left = `-${now}%`;
+    }
+}, 2000);
+
+function trangthai() {
+    document.getElementById('historyorder').style.top = '100%';
+    document.getElementById('status').style.top = '0%';
+    document.getElementById('buttontrangthai').classList.add('activeeselect');
+    document.getElementById('buttonlsm').classList.remove('activeeselect');
+}
+
+function lichsumua() {
+    document.getElementById('historyorder').style.top = '0%';
+    document.getElementById('status').style.top = '100%';
+    document.getElementById('buttontrangthai').classList.remove('activeeselect');
+    document.getElementById('buttonlsm').classList.add('activeeselect');
+}
+
+function trovetaikhoan() {
+    document.getElementById('inforcart').style.transform = 'scaleY(0)';
+}
+
+function xemchitiet(){
+    document.getElementById('inforcart').style.transform = 'scaleY(1)';
+}
+
+
