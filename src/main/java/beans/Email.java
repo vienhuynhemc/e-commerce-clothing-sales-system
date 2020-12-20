75de22d0546a645e67b382159ca9da7188e60396
vientamthuong <18130281@st.hcmuaf.edu.vn>
2020-12-20 10:40:12 +0700

    github oke

---
+++ src/main/java/beans/Email.java
@@ -0,0 +1,8 @@
+package beans;
+
+public class Email {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Email");
+    }
+}
--- src/main/java/database/HelloWorld.java
+++ src/main/java/database/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello Database");
+        System.out.println("Test casi nuwxa ne");
     }
+
 }
