79d78a85277210adfed144254ebcfcc6bdbd8ca1
vientamthuong <18130281@st.hcmuaf.edu.vn>
2020-12-24 18:56:26 +0700

    Test chức năng ngôn ngữ

---
+++ src/main/java/model/FooterLanguageModel.java
@@ -0,0 +1,49 @@
+package model;
+
+import beans.FooterLanguage;
+
+import java.util.Map;
+
+public class FooterLanguageModel {
+
+    //  Tạo thể hiện cho lớp
+    private static FooterLanguageModel footerLanguageModel;
+
+    //  Class này có một thằng beans là footerlanguage
+    private FooterLanguage footerLanguage;
+
+    //  Constructor tạo beaans
+    private FooterLanguageModel() {
+
+        footerLanguage = new FooterLanguage();
+
+    }
+
+    //  Phương thức lấy thể hiện
+    public static FooterLanguageModel getInstance() {
+
+        //  Kiểm tra xem thể hiện có null hay không, null thì tạo mới
+        if (footerLanguageModel == null) {
+            footerLanguageModel = new FooterLanguageModel();
+        }
+
+        //  Trả về thể hiện
+        return footerLanguageModel;
+
+    }
+
+    //  Phương thức lấy về danh sách ngôn ngữ
+    public Map<String, String> getList(String lang) {
+
+        //  Nếu là english thì trả về danh sách ngôn ngữ tiếng anh
+        if (lang.equals("english")) {
+            return footerLanguage.getEnglish();
+        }
+
+        //  Không thì danh sách ngôn ngữ tiếng việt
+        else {
+            return footerLanguage.getVietnamese();
+        }
+    }
+
+}
