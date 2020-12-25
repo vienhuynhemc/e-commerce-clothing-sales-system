3274bc6d0a44a250c03f18d86e2c15942d550392
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2020-12-25 21:56:41 +0700

    thao tác CSDL với category

---
+++ src/main/java/controllerIndex/CategoryController.java
@@ -0,0 +1,18 @@
+package controllerIndex;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CategoryController", urlPatterns = "/CategoryController")
+public class CategoryController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
